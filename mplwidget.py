cfabd8d4db08bb4629eb23a44c714afc1fd01b8e
Rounak Paul <rounakpaul@hotmail.com>
2023-04-13 13:01:49 +0530

    Update mplwidget.py

--- mplwidget.py
+++ mplwidget.py
@@ -1,20 +1,20 @@
-from PyQt5.QtWidgets import*
-
+from PyQt6.QtWidgets import*
 from  matplotlib.backends.backend_qt5agg  import  FigureCanvas
-
 from  matplotlib.figure  import  Figure
+from  matplotlib.backends.backend_qt5agg  import  ( NavigationToolbar2QT  as  NavigationToolbar )
 
-    
 class MplWidget(QWidget):
-    
+    '''
+    TODO
+    '''
     def __init__(self, parent = None):
-
-        QWidget.__init__( self ,  parent )
+        QWidget.__init__(self, parent)
         
         self.canvas = FigureCanvas(Figure())
-        
         vertical_layout = QVBoxLayout()
         vertical_layout.addWidget(self.canvas)
-        
         self.canvas.axes = self.canvas.figure.add_subplot(111)
         self.setLayout(vertical_layout)
+        self.mplToolBar = NavigationToolbar(self.canvas, self)
+        self.layout().addWidget(self.mplToolBar)
+        
